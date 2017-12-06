4826e0c3996e7cf3fc976a0b16129b76971c7c9a
tolalur <tolalur@gmail.com>
2017-12-07 01:26:05 +0300

    style

--- give_back_data.php
+++ give_back_data.php
@@ -15,14 +15,65 @@ if ($deploy_id == 1)
     $response['tasks'] = array(
         array(
             'number' => 'ATGSM-7355',
-            'title' => '',
-            'description' => '',
-            'performer' => '',
-            'type' => '',
-            'priority' => '',
-            'product_version' => '',
-            'status' => ''
+            'title' => 'Использование переменных в настройках СЕО',
+            'description' => 'Описание задачи ATGSM-7355',
+            'performer' => 'Бородин Алексей',
+            'type' => 'task',
+            'priority' => 'low',
+            'product_version' => 'deploy 1',
+            'status' => 'refinement'
         ),
+        array(
+            'number' => 'ATGSM-6886',
+            'title' => 'Починить поиск по бренду',
+            'description' => 'Описание задачи ATGSM-6886',
+            'performer' => 'Бутаков Валентин',
+            'type' => 'error',
+            'priority' => 'medium',
+            'product_version' => 'deploy 1',
+            'status' => 'open'
+        ),
+        array(
+            'number' => 'ATGSM-7192',
+            'title' => 'Создание формы регистрации',
+            'description' => 'Описание задачи ATGSM-7192',
+            'performer' => 'Власов Артем',
+            'type' => 'error',
+            'priority' => 'high',
+            'product_version' => 'deploy 1',
+            'status' => 'in_work'
+        ),
+
+        array(
+            'number' => 'ATGSM-7355',
+            'title' => 'Использование переменных в настройках СЕО',
+            'description' => 'Описание задачи ATGSM-7355',
+            'performer' => 'Бородин Алексей',
+            'type' => 'task',
+            'priority' => 'low',
+            'product_version' => 'deploy 1',
+            'status' => 'refinement'
+        ),
+        array(
+            'number' => 'ATGSM-6886',
+            'title' => 'Починить поиск по бренду',
+            'description' => 'Описание задачи ATGSM-6886',
+            'performer' => 'Бутаков Валентин',
+            'type' => 'error',
+            'priority' => 'medium',
+            'product_version' => 'deploy 1',
+            'status' => 'open'
+        ),
+        array(
+            'number' => 'ATGSM-7192',
+            'title' => 'Создание формы регистрации',
+            'description' => 'Описание задачи ATGSM-7192',
+            'performer' => 'Власов Артем',
+            'type' => 'error',
+            'priority' => 'high',
+            'product_version' => 'deploy 1',
+            'status' => 'in_work'
+        )
     );
 }
 
